f18702ba751388290e3f0164374a4da180805ac3
JaviAlbe <64216714+JaviAlbe@users.noreply.github.com>
2026-06-10 14:20:18 +0200

    update App.jsx scaffold for new challenges

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
 import "./index.css";
 
-// Challenge 1: Import and use your custom hook here.
-// Challenge 2: Import and use PokemonCard here.
-// Challenge 3: Import and use usePokemonContext to read the selected Pokémon.
+// Challenge 1: Import PokemonCard and render the list of 151 Pokémon.
+//              The list must be alphabetically sorted and each name capitalised.
+// Challenge 2: Add a search bar with debounce to filter by name.
+// Challenge 3: Import usePokemonContext to read selectedPokemon and
+//              highlight the selected card differently.
 
 function App() {
-  // TODO: use your custom hook to get pokemonList, loading, and error
-
-  // Bonus: add a search input to filter the list by name
-
   return (
     <div>
       <h1>Pokédex</h1>
 
-      {/* Challenge 3: Render a selected Pokémon panel here */}
+      {/* Challenge 2: search bar goes here */}
 
-      {/* Bonus: Render a search input here */}
-
-      {/* Render loading state */}
-      {/* Render error state */}
-      {/* Render the list of PokemonCards */}
+      {/* Challenge 1 + 3: render the list of PokemonCards here */}
     </div>
   );
 }
 
 export default App;
+
