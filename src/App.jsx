c0fed053a053071c088ea74802fde7f7a5cd71fa
JaviAlbe <64216714+JaviAlbe@users.noreply.github.com>
2026-06-11 12:28:44 +0200

    render cards in App.jsx from pokemon.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "./index.css";
+import pokemonNames from "./pokemon.json";
+import { PokemonCard } from "./components/PokemonCard";
 
 function App() {
   return (
     <div>
       <h1>Pokédex</h1>
+      <div>
+        {pokemonNames.map((name) => (
+          <PokemonCard key={name} name={name} onClick={() => {}} />
+        ))}
+      </div>
     </div>
   );
 }
